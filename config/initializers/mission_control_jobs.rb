5fef70c90382299c783aceb3c6b842ccd888d29f
Phoenix <phoenix@playgrounds.dev>
2026-03-15 16:14:14 +0000

    fix: correct MissionControl::Jobs auth method name (http_basic_auth_user)

--- config/initializers/mission_control_jobs.rb
+++ config/initializers/mission_control_jobs.rb
@@ -1,3 +1,3 @@
 MissionControl::Jobs.http_basic_auth_enabled = true
-MissionControl::Jobs.http_basic_auth_username = ENV.fetch("JOBS_DASHBOARD_USERNAME", "admin")
+MissionControl::Jobs.http_basic_auth_user = ENV.fetch("JOBS_DASHBOARD_USERNAME", "admin")
 MissionControl::Jobs.http_basic_auth_password = ENV.fetch("JOBS_DASHBOARD_PASSWORD", "secret")
